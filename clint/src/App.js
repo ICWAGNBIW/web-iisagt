0fdaa0745a3695f2e15b6c61e8762442c19b2f41
ICWAGNBIW <ICWAGN@gmail.com>
2022-08-03 00:41:36 +0300

    lil update

--- clint/src/App.js
+++ clint/src/App.js
@@ -1,12 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+//import './App.css';
+import {Routes, Route} from 'react-router-dom';
+import {useContext} from 'react';
+import {Context} from './index';
 
 function App() {
+  //let isAuth = true;
+  const {user} = useContext(Context);
+  console.log(user);
   return (
     <div className="App">
-     HEY
+     <Routes>
+      <Route path="/hey" element={<Hey/>}/>
+     {user.isAuth && <Route path="/adminhey" element={<AdminHey/>}/>}
+      </Routes>
     </div>
   );
+}
+
+function Hey () {
+  return (
+    <div>HEY!!!!</div>
+  )
+}
+
+
+function AdminHey () {
+  return (
+    <div>HEY BUT FOR ADMIN C:</div>
+  )
 }
 
 export default App;
---
+++ clint/src/http/index.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+const $host = axios.create({
+
+})
+
+
+const $authHost = axios.create({
+    
+})
